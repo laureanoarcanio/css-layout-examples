8d2561fc103a43665f8bad8d9463f05dbb360368
Laureano Arcanio <larcanio@gmail.com>
2015-07-04 00:52:55 -0300

    Implemet navbar toggle and subnav close in all 3 layouts

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,8 +2,16 @@
 
   'use strict';
 
-  $(function () {
-    // FireShell
+  $('#subnav .fa-close').on('click', function() {
+    $(this).parent().addClass('hidden');
   });
 
+  $('#nav .fa-user').on('click', function() {
+    $('#subnav').toggleClass('hidden');
+  });
+
+  $('#nav').on('click', 'li.nav-toggle', function() {
+    $('#nav, #subnav, #main').toggleClass('expanded');
+  });
+  
 })(jQuery, window, document);
